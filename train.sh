693dd9a2b4e6f9c6ce8a92ce013bec9ff09e2b9b
nftblackmagic <108916776+nftblackmagic@users.noreply.github.com>
2023-10-05 11:41:50 -0700

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,1 +1,5 @@
-python -u main.py --logdir models/dci-vton --pretrained_model checkpoints/model.ckpt --base configs/viton512.yaml --scale_lr False
+python -u main.py \
+--logdir models/Paint-by-Example \
+--base configs/viton512.yaml \
+--pretrained_model models/checkpoints/v1-5-pruned-emaonly-modified-9channels.ckpt \
+--scale_lr False
