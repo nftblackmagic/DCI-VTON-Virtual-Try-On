0ddba8939df2dacf1ff36c288b4c05ebbb76f098
clothai <145078323+clothai@users.noreply.github.com>
2023-10-05 08:01:44 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt update
+apt install -y aria2
+apt-get install python3-pip
+apt install vim
+apt-get install tmux
+
+pip install gdown
+pip install --upgrade gdown
+apt-get install unzip
